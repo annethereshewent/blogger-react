3a98b87ca5b04231c89d5e20403ff745c97d0efe
Anne Castrillon <anne.castrillon@gmail.com>
2022-09-06 00:18:05 -0700

    modify Login component styles

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -9,14 +9,14 @@ export function Login() {
   return  (
     <Container id="login-main">
       <Grid container>
-      <Logo />
-      <Grid id="existing-user-section" xs={5} item>
-        <UserAccounts />
-      </Grid>
-      <Grid sm={12} lg={5} item>
-        <Grid id="login-column" lg={10} sm={6} item>
-          <LoginCard />
+        <Logo />
+        <Grid id="existing-user-section" xs={5} item>
+          <UserAccounts />
         </Grid>
+        <Grid sm={12} lg={5} item>
+          <Grid id="login-column" lg={10} sm={6} item>
+            <LoginCard />
+          </Grid>
         </Grid>
       </Grid>
     </Container>
