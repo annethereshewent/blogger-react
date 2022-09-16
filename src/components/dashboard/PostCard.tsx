8dd6a64b691f1ca7d8500a1a5734ad7407c53f7e
Anne Castrillon <anne.castrillon@gmail.com>
2022-09-15 22:18:49 -0700

    add PostCard component and some minor refactoring

---
+++ src/components/dashboard/PostCard.tsx
@@ -0,0 +1,46 @@
+import { AddCommentRounded, FavoriteOutlined, ReplyOutlined } from "@mui/icons-material";
+import { Avatar, IconButton } from "@mui/material";
+import linkifyHtml from "linkifyjs/lib/linkify-html";
+import { Post } from "../../types/Post";
+
+
+
+interface PostCardProps {
+  post: Post
+}
+
+export function PostCard({post}: PostCardProps) {
+
+  /*
+  * Replaces new lines with br tags and auto links urls.
+  * @TODO: sanitization already happens
+  * on the backend, optionally add it to client side
+  */
+   function convertPost(body: string): string {
+
+    return linkifyHtml(body.replace(/\n/g, "<br/>"), { target: '_blank' })
+  }
+
+  return (
+    <div className="post-card">
+      <div className="post">
+        <Avatar src={post.avatar} className="post-avatar" />
+        <p
+          className="post-body"
+          dangerouslySetInnerHTML={{ __html: convertPost(post.body) }}
+        />
+      </div>
+      <div className="post-actions">
+        <IconButton className="icon-button">
+          <AddCommentRounded />
+        </IconButton>
+        <IconButton className="icon-button">
+          < ReplyOutlined />
+        </IconButton>
+        <IconButton className="icon-button">
+          <FavoriteOutlined />
+        </IconButton>
+      </div>
+    </div>
+  )
+}
--- src/components/dashboard/PostsContainer.tsx
+++ src/components/dashboard/PostsContainer.tsx
@@ -1,7 +1,5 @@
 import { Post } from "../../types/Post"
-import linkifyHtml from 'linkify-html'
-import { Avatar, IconButton } from "@mui/material"
-import { ReplyOutlined, FavoriteOutlined, AddCommentRounded } from '@mui/icons-material'
+import { PostCard } from "./PostCard"
 
 interface PostsContainerProps {
   posts: Post[]
@@ -9,42 +7,11 @@ interface PostsContainerProps {
 
 export function PostsContainer({ posts }: PostsContainerProps) {
 
-
-  /*
-  * Replaces new lines with br tags and auto links urls.
-  * @TODO: sanitization already happens
-  * on the backend, optionally add it to client side
-  */
-  function convertPost(body: string): string {
-
-    return linkifyHtml(body.replace(/\n/g, "<br/>"), { target: '_blank' })
-  }
-
   return (
     <div id="posts-container">
       { posts.map(post => {
         return (
-          <div className="post-card">
-            <div className="post">
-              <Avatar src={post.avatar} className="post-avatar" />
-              <p
-                 className="post-body"
-                 key={post.id}
-                 dangerouslySetInnerHTML={{ __html: convertPost(post.body) }}
-              />
-            </div>
-            <div className="post-actions">
-              <IconButton className="icon-button">
-                <AddCommentRounded />
-              </IconButton>
-              <IconButton className="icon-button">
-                < ReplyOutlined />
-              </IconButton>
-              <IconButton className="icon-button">
-                <FavoriteOutlined />
-              </IconButton>
-            </div>
-          </div>
+          <PostCard key={post.id} post={post} />
         )
       })}
     </div>
