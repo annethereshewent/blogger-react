1ea529b30369b9c5c7219fad5945ae2095a553df
Anne Castrillon <anne.castrillon@gmail.com>
2022-09-17 22:57:31 -0700

    fix up dashboard logo

--- src/components/dashboard/DashboardLogo.tsx
+++ src/components/dashboard/DashboardLogo.tsx
@@ -3,11 +3,14 @@ import '../../styles/logo.scss'
 export function DashboardLogo() {
 
   return (
-    <h1 id="title" className="dashboard">
-      <span className="bracket">[</span>
-      <span className="title-text">B</span>
-      <span className="bracket">]</span>
-      <span className="period">.</span>
-    </h1>
+    <div>
+      <h1 id="title" className="dashboard">
+        <span className="bracket">[</span>
+        <span className="title-text">B</span>
+        <span className="bracket">]</span>
+        <span className="period">.</span>
+      </h1>
+      <div style={{clear: 'right'}} />
+    </div>
   )
 }
