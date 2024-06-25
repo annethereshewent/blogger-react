d5e2169be073846c8ce5eae07e1dc91c6fe6ad08
Anne Castrillon <anne.castrillon@gmail.com>
2024-06-25 11:07:26 -0700

    refactor the confirm delete modal into its own component

--- src/components/dashboard/posts/PostCardMenu.tsx
+++ src/components/dashboard/posts/PostCardMenu.tsx
@@ -14,6 +14,7 @@ import { DashboardService } from '../../../services/DashboardService'
 import { DeletedPost, Post } from '../../../types/post/Post'
 import { User } from '../../../types/user/User'
 import { modalStyle } from '../../../util/modalStyles'
+import { ConfirmDeleteModal } from './ConfirmDeleteModal'
 
 interface PostCardMenuProps {
   post: Post
@@ -26,48 +27,10 @@ interface PostCardMenuProps {
 export function PostCardMenu({ post, posts, user, setPosts, setPost }: PostCardMenuProps) {
   const anchorRef = useRef<HTMLDivElement>(null)
   const [menuOpen, setMenuOpen] = useState(false)
-  const [loading, setLoading] = useState(false)
   const [modalOpen, setModalOpen] = useState(false)
 
   function confirmDelete() {
     setModalOpen(true)
-  }
-
-  async function deletePost() {
-    try {
-      if (!post.deleted) {
-        setLoading(true)
-        const result = await new DashboardService().deletePost(post.id)
-
-        const { data } = result
-
-        if (data.success) {
-          if (setPosts != null) {
-            // should be ok to use '!!' here if setPosts is not null.
-            const postsCopy = [...posts!!]
-
-            const i = postsCopy.indexOf(post)
-
-            if (i !== -1) {
-              console.log('i isnt equal to -1')
-              postsCopy.splice(i, 1)
-
-              setPosts(postsCopy)
-            }
-          } else if (setPost != null) {
-            const deletedPost: DeletedPost = { deleted: true }
-
-            setPost(deletedPost)
-          }
-        }
-      }
-    } catch (e: any) {
-      console.log(e)
-    } finally {
-      setLoading(false)
-      setModalOpen(false)
-      setMenuOpen(false)
-    }
   }
 
   function openMenu() {
@@ -75,11 +38,6 @@ export function PostCardMenu({ post, posts, user, setPosts, setPost }: PostCardM
   }
 
   function closeMenu() {
-    setMenuOpen(false)
-  }
-
-  function handleClose() {
-    setModalOpen(false)
     setMenuOpen(false)
   }
 
@@ -112,26 +70,15 @@ export function PostCardMenu({ post, posts, user, setPosts, setPost }: PostCardM
           </Menu>
         </div>
       )}
-      {/* @TODO: move this to its own component */}
-      <Modal id="delete-post-modal" open={modalOpen} onClose={handleClose}>
-        <Card className="confirm-delete" style={modalStyle}>
-          <CardContent>
-            <h4 className="delete-heading">Confirm delete</h4>
-            <p className="delete-body">
-              Are you sure you want to delete this post? <br />
-              This action cannot be undone!
-            </p>
-          </CardContent>
-          <CardActions className="delete-actions">
-            <Button type="button" variant="contained" color="error" onClick={deletePost}>
-              Delete
-            </Button>
-            <Button type="button" variant="contained" color="primary" onClick={handleClose}>
-              Cancel
-            </Button>
-          </CardActions>
-        </Card>
-      </Modal>
+      <ConfirmDeleteModal
+        setPost={setPost}
+        setPosts={setPosts}
+        modalOpen={modalOpen}
+        post={post}
+        posts={posts}
+        setMenuOpen={setMenuOpen}
+        setModalOpen={setModalOpen}
+      />
     </div>
   )
 }
